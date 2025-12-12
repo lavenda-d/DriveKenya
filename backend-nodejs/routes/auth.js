1d385ea80adc71cb8c05ce06f2c34ad19be7e83a
lavenda-d <lavendadoris20@gmail.com>
2025-12-12 14:44:24 +0300

    Fix: import nodemailer in backend auth route

--- backend-nodejs/routes/auth.js
+++ backend-nodejs/routes/auth.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
+import nodemailer from 'nodemailer';
 import { body, validationResult } from 'express-validator';
 import { query } from '../config/database-sqlite.js';
 import { authenticateToken } from '../middleware/auth.js';
